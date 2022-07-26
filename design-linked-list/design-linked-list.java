a0be3e6917cb8a4109e04dc4dfb42ff89ec3b5a5
Tushar K <61900832+Tushar1729-k@users.noreply.github.com>
2022-07-25 20:36:06 -0700

    Time: 8 ms, Memory: 50.8 MB - LeetHub

---
+++ design-linked-list/design-linked-list.java
@@ -0,0 +1,149 @@
+class MyLinkedList {
+
+  int size;
+  Node head;
+
+  public MyLinkedList() {
+    size = 0;
+    head = null;
+  }
+  
+  public int get(int index) {
+    if (index >= size || index < 0)
+      return -1;
+    if (index == 0)
+      return head.val;
+    if (index == size-1)
+      return head.prev.val;
+    if (index < size/2 || (size%2==1 && index==size/2)) {
+      Node forwardTmp = head;
+      for (int i = 0; i < index; i++)
+        forwardTmp = forwardTmp.next;
+      return forwardTmp.val;
+    }
+    else {
+      Node backwardTmp = head.prev;
+      for (int i = size-1; i > index; i--)
+        backwardTmp = backwardTmp.prev;
+      return backwardTmp.val;
+    }
+  }
+  
+  public void addAtHead(int val) {
+    Node newHead = new Node(val);
+    newHead.next = head;
+    if (head != null) {
+      newHead.prev = head.prev;
+      if (head.prev != null)
+        head.prev.next = newHead;
+      head.prev = newHead;
+    }
+    else {
+      newHead.next = newHead;
+      newHead.prev = newHead;
+    }
+    head = newHead;
+    size++;
+  }
+  
+  public void addAtTail(int val) {
+    Node newTail = new Node(val);
+    newTail.next = head;
+    if (head != null) {
+      newTail.prev = head.prev;
+      // no tail before
+      if (head.prev == null) {
+        newTail.prev = head;
+        head.next = newTail;
+      }
+      if (head.prev != null)
+        head.prev.next = newTail;
+      head.prev = newTail;
+    }
+    else
+      head = newTail;
+    size++;
+  }
+  
+  public void addAtIndex(int index, int val) {
+    Node newNode = new Node(val);
+    if (index == 0) {
+      addAtHead(val);
+      return;
+    }
+    if (index == size) {
+      addAtTail(val);
+      return;
+    }
+    if (index > size)
+      return;
+    if (index < size/2 || (size%2==1 && index==size/2)) {
+      Node forwardTmp = head;
+      for (int i = 0; i < index-1; forwardTmp = forwardTmp.next)
+        i++;
+      newNode.next = forwardTmp.next;
+      if (forwardTmp.next != null)
+        forwardTmp.next.prev = newNode;
+      newNode.prev = forwardTmp;
+      forwardTmp.next = newNode;
+    }
+    else {
+      Node backwardTmp = head.prev;
+      for (int i = size-1; i > index; backwardTmp = backwardTmp.prev)
+        i--;
+      newNode.prev = backwardTmp.prev;
+      if (backwardTmp.prev != null)
+        backwardTmp.prev.next = newNode;
+      newNode.next = backwardTmp;
+      backwardTmp.prev = newNode;
+    }
+    size++;
+  }
+  
+  public void deleteAtIndex(int index) {
+    if (index < 0 || index >= size)
+      return;
+    // removeHead
+    if (index == 0) {
+      if (head == null)
+        return;
+      if (head.prev != null)
+        head.prev.next = head.next;
+      if (head.next != null)
+        head.next.prev = head.prev;
+      head = head.next;
+      size--;
+      return;
+    } 
+    // removeTail
+    else if (index == size-1) {
+      head.prev = head.prev.prev;
+      head.prev.next = head;
+      size--;
+      return;
+    }
+    if (index < size/2 || (size%2==1 && index==size/2)) {
+      Node forwardTmp = head;
+      for (int i = 0; i < index-1; i++)
+        forwardTmp = forwardTmp.next;
+      forwardTmp.next = forwardTmp.next.next;
+      forwardTmp.next.prev = forwardTmp;
+    }
+    else {
+      Node backwardTmp = head.prev;
+      for (int i = size-1; i > index+1; i--)
+        backwardTmp = backwardTmp.prev;
+      backwardTmp.prev = backwardTmp.prev.prev;
+      backwardTmp.prev.next = backwardTmp;
+    }
+    size--;
+  }
+
+  private class Node {
+    int val;
+    Node next;
+    Node prev;
+    Node(int x) { val = x; }
+  }
+
+}
