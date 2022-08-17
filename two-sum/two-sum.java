4c7b453941b564f5c045a560a5033f94c069dca8
Tushar K <61900832+Tushar1729-k@users.noreply.github.com>
2022-08-17 12:33:13 -0500

    Time: 9 ms, Memory: 47 MB - LeetHub

--- two-sum/two-sum.java
+++ two-sum/two-sum.java
@@ -1,13 +1,16 @@
 class Solution {
   public int[] twoSum(int[] nums, int target) {
 
+    // List<Integer> numsList = Arrays.stream(int).boxed().toList();
     Map<Integer, Integer> numToPos = new HashMap<>();
+    for (int i = 0; i < nums.length; i++)
+      numToPos.put(nums[i], i);
+    // will overwrite pos if num is duplicate
     for (int i = 0; i < nums.length-1; i++) {
       int compl = target - nums[i];
-      for (int j = i+1; j < nums.length; j++) {
-        if (nums[j] == compl)
-          return new int[]{i,j};
-      }
+      Integer compPos = numToPos.get(compl);
+      if (compPos != null && compPos != i)
+        return new int[]{i,compPos};
     }
     return new int[]{0,1};
   }
